285aa527ee08bc82a65eb62a03489aa97fc3d348
Lisa-kode <54777889+Lisa-kode@users.noreply.github.com>
2019-09-09 08:15:11 +0200

    Add files via upload

---
+++ Øvelsesopgaver fra kap. 1 Java-bog/src/Kapitel1.java
@@ -0,0 +1,65 @@
+public class Kapitel1 {
+
+    //Opgave 1
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java now");
+        System.out.println("Programing is fun");
+        System.out.println(" ");
+
+        //Opgave 2
+
+        /*forfejlede forsøg:
+
+        System.out.println("5 * I love Java");
+
+        String x = "I love Java";
+        String repeated = x.repeat(5);
+
+         */
+
+        System.out.println("I love Java");System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println(" ");
+
+        //Opgave 1.4
+
+        System.out.println("a   a^2     a^3     a^4");
+        System.out.println("1   1       1       1");
+        System.out.println("2   4       8       16");
+        System.out.println("3   9       27      81");
+        System.out.println("4   16      64      256");
+        System.out.println(" ");
+
+         /*kunne det her være en anden metode?? Sætte variabel 'a' ind og lave den
+         til en lille lommeregner.
+        int a = 2
+        int sum1 = a^2
+         */
+
+
+        //Opgave 1.5
+
+        System.out.print("(7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5) = ");
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
+        System.out.println(" ");
+
+        //Opgave 1.6
+
+        System.out.print("1+2+3+4+5+6+7+8+9+10 = ");
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        System.out.println(" ");
+
+
+        //Opgave 1.12
+
+        System.out.print("The runner has run ");
+        System.out.print(24/1.4035);
+        System.out.println(" miles pr hour");
+
+
+    }
+}
